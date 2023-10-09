2af73fb0c47d2eb48d93f099a12f0019fb1e96ee
Atheer-95 <athos@outlook.sa>
2023-10-09 14:18:02 +0300

    adding the project

---
+++ CIBPay textField/CIBPay textField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CIBPay textField
+//
+//  Created by Atheer Othman on 24/03/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
